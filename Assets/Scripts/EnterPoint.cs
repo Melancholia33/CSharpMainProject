ba9ff64a12c32fb514264b93021d682346d1c2f5
Mosin Evgeny <mozzinmozzin@yandex.ru>
2024-06-02 22:53:03 +0300

    comment
    study commit checkout

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,11 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    //hello
+    /*/
+     * sup??
+     */
+
     void Start()
     {
         Time.timeScale = _timeScale;
